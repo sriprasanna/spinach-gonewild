32ae72c4ea44cef67d3450af7fdb4c63183635a2
Sri Prasanna <k.sriprasanna@gmail.com>
2012-02-24 18:10:08 +0530

    initial commit

---
+++ lib/spinach-gonewild.rb
@@ -0,0 +1,7 @@
+require "spinach-gonewild/version"
+
+module Spinach
+  module Gonewild
+    # Your code goes here...
+  end
+end
---
+++ lib/spinach-gonewild/version.rb
@@ -0,0 +1,5 @@
+module Spinach
+  module Gonewild
+    VERSION = "0.0.1"
+  end
+end
